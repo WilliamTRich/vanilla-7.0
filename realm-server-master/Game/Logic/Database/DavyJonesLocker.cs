42a93be806847c172bdadfcf2aa54acb28c153c2
itsEvil <56killer65@gmail.com>
2023-04-22 07:28:42 +0100

    Added Davy Jones boss Behavior

--- realm-server-master/Game/Logic/Database/DavyJonesLocker.cs
+++ realm-server-master/Game/Logic/Database/DavyJonesLocker.cs
@@ -1,4 +1,5 @@
-﻿using RotMG.Game.Logic.Behaviors;
+﻿using RotMG.Common;
+using RotMG.Game.Logic.Behaviors;
 using RotMG.Game.Logic.Loots;
 using RotMG.Game.Logic.Transitions;
 
@@ -8,6 +9,79 @@ namespace RotMG.Game.Logic.Database
     {
         public void Init(BehaviorDb db)
         {
+            db.Init("Davy Jones",
+                    new DropPortalOnDeath("Glowing Realm Portal"),
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable),
+                    new State("Floating",
+                        new ChangeSize(100, 100),
+                        new SetAltTexture(3),
+                        new Wander(.6f),
+                        new StayCloseToSpawn(0.2f, 8),
+                        new Shoot(10, 5, 10, 0, cooldown: 1000),
+                        new Shoot(10, 1, 10, 1, cooldown: 2000),
+                        new EntityWithinTransition("Ghost Lanturn On", 30, "CheckOffLanterns")
+                        ),
+                    new State("CheckOffLanterns",
+                        new SetAltTexture(2),
+                        new ReturnToSpawn(1),
+                        new Shoot(10, 5, 10, 0, cooldown: 1000, predictive: 0.5f),
+                        new Shoot(10, 1, 10, 1, cooldown: 500),
+                        new EntitiesNotWithinTransition(30, targetState: "Vunerable", "Ghost Lanturn Off")
+                        ),
+                    new State("Vunerable",
+                        new ConditionalEffect(ConditionEffectIndex.Invulnerable, false, 0),
+                        new OrderOnce(99, "Ghost Lanturn On", "Stay Here"),
+                        new SetAltTexture(5, 6, 500, loop: true),
+                        new TimedTransition(2500, "deactivate 1")
+                        ),
+                    new State("deactivate 1",
+                        new ConditionalEffect(ConditionEffectIndex.Invulnerable, true, -1),
+                        new SetAltTexture(4),
+                        new OrderOnce(99, "Ghost Lanturn On", "Shoot"),
+                        new TimedTransition(3500, "Floating")
+                        ),
+                new Threshold(0.01f,
+                    new ItemLoot("Potion of Wisdom", 0.80f, 3),
+                    new ItemLoot("Potion of Wisdom", 0.80f, 3),
+                    new ItemLoot("Potion of Wisdom", 1),
+                    new ItemLoot("Ghost Pirate Rum", 0.4f),
+                    new ItemLoot("Ghost Pirate Rum", 0.3f),
+                    new ItemLoot("Ghost Pirate Rum", 0.2f),
+                    new TierLoot(8, TierLoot.LootType.Weapon, 0.25f),
+                    new TierLoot(9, TierLoot.LootType.Weapon, 0.125f),
+                    new TierLoot(8, TierLoot.LootType.Armor, 0.25f),
+                    new TierLoot(9, TierLoot.LootType.Armor, 0.125f),
+                    new TierLoot(3, TierLoot.LootType.Ability, 0.25f),
+                    new TierLoot(4, TierLoot.LootType.Ability, 0.125f),
+                    new TierLoot(4, TierLoot.LootType.Ring, 0.125f),
+                    new TierLoot(10, TierLoot.LootType.Armor, 0.125f),
+                    new TierLoot(5, TierLoot.LootType.Ability, 0.0625f),
+                    new ItemLoot("Captain's Ring", 0.01f),
+                    new ItemLoot("Wine Cellar Incantation", 0.05f),
+                    new ItemLoot("Spectral Cloth Armor", 0.01f),
+                    new ItemLoot("Spirit Dagger", 0.01f),
+                    new ItemLoot("Ghostly Prism", 0.01f)
+                    ));
+
+            db.Init("Ghost Lanturn Off",
+                new State("base",
+                    new TransformOnDeath("Ghost Lanturn On")
+                ));
+            db.Init("Ghost Lanturn On",
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable),
+                    new State("idle",
+                        new TimedTransition(8000, "gone")
+                        ),
+                    new State("Stay Here"),
+                    new State("Shoot",
+                        new Shoot(10, 6, cooldown: 9000001, cooldownOffset: 100),
+                        new TimedTransition(250, "gone")
+                        ),
+                    new State("gone",
+                        new Transform("Ghost Lanturn Off")
+                        )
+                    );
+
             db.Init("Purple Key",
                 new State("Idle",
                     new PlayerWithinTransition(1.5f, true, "Consume")
