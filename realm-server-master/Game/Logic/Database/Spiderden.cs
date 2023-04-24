b8440565c0c5cde95644ceb2829b1527f9bfdc34
itsEvil <56killer65@gmail.com>
2023-04-24 11:21:00 +0100

    Added missing behaviours

--- realm-server-master/Game/Logic/Database/Spiderden.cs
+++ realm-server-master/Game/Logic/Database/Spiderden.cs
@@ -15,6 +15,15 @@ namespace RotMG.Game.Logic.Database
         public void Init(BehaviorDb db)
         {
             db.Init("Arachna the Spider Queen",
+                new DropPortalOnDeath("Glowing Realm Portal"),
+                new OrderOnDeath(30, "Arachna Web Spoke 1", "Die", 1),
+                new OrderOnDeath(30, "Arachna Web Spoke 2", "Die", 1),
+                new OrderOnDeath(30, "Arachna Web Spoke 3", "Die", 1),
+                new OrderOnDeath(30, "Arachna Web Spoke 4", "Die", 1),
+                new OrderOnDeath(30, "Arachna Web Spoke 5", "Die", 1),
+                new OrderOnDeath(30, "Arachna Web Spoke 6", "Die", 1),
+                new OrderOnDeath(30, "Arachna Web Spoke 7", "Die", 1),
+                new OrderOnDeath(30, "Arachna Web Spoke 8", "Die", 1),
                 new State("WaitForPlayers",
                     new PlayerWithinTransition(Player.SightRadius, true, "Start")
                     ),
@@ -33,41 +42,77 @@ namespace RotMG.Game.Logic.Database
                 new State("Attack",  
                     new Wander(.3f),
                     new Follow(.9f),
-                    new Shoot(Player.SightRadius, 3, 0, 0)
+                    new Shoot(Player.SightRadius, 3, 0, 0),
+                    new TossObject("Black Den Spider", 5, 17, 5000),
+                    new TossObject("Brown Den Spider", 5, 17, 5000, 0, 2000),
+                    new TossObject("Black Spotted Den Spider", 5, 17, 5000, 0, 4000),
+                    new TossObject("Red Spotted Den Spider", 5, 17, 5000, 0, 6000)
                 )
                 );
 
             db.Init("Arachna Web Spoke 1",
                 new State("Start", 
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable, true),
                     new Shoot(Player.SightRadius, 1, 0, 0, 180, cooldown: 100)
+                ),
+                new State("Die",
+                    new Decay(0)
                 ));
             db.Init("Arachna Web Spoke 2",
                 new State("Start",
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable, true),
                     new Shoot(Player.SightRadius, 1, 0, 0, 225, cooldown: 100)
+                ),
+                new State("Die",
+                    new Decay(0)
                 ));
             db.Init("Arachna Web Spoke 3",
                 new State("Start",
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable, true),
                     new Shoot(Player.SightRadius, 1, 0, 0, -90, cooldown: 100)
+                ),
+                new State("Die",
+                    new Decay(0)
                 ));
             db.Init("Arachna Web Spoke 4",
                 new State("Start",
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable, true),
                     new Shoot(Player.SightRadius, 1, 0, 0, -45, cooldown: 100)
-                ));            
+                ),
+                new State("Die",
+                    new Decay(0)
+                ));
             db.Init("Arachna Web Spoke 5",
                 new State("Start",
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable, true),
                     new Shoot(Player.SightRadius, 1, 0, 0, 0, cooldown: 100)
-                ));            
+                ),
+                new State("Die",
+                    new Decay(0)
+                ));
             db.Init("Arachna Web Spoke 6",
                 new State("Start",
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable, true),
                     new Shoot(Player.SightRadius, 1, 0, 0, 45, cooldown: 100)
-                ));            
+                ),
+                new State("Die",
+                    new Decay(0)
+                ));
             db.Init("Arachna Web Spoke 7",
                 new State("Start",
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable, true),
                     new Shoot(Player.SightRadius, 1, 0, 0, 90, cooldown: 100)
-                ));            
+                ),
+                new State("Die",
+                    new Decay(0)
+                ));
             db.Init("Arachna Web Spoke 8",
                 new State("Start",
+                    new ConditionalEffect(ConditionEffectIndex.Invulnerable, true),
                     new Shoot(Player.SightRadius, 1, 0, 0, 135, cooldown: 100)
+                ),
+                new State("Die",
+                    new Decay(0)
                 ));
             db.Init("Spider Egg Sac",
                 
@@ -86,10 +131,74 @@ namespace RotMG.Game.Logic.Database
 
             db.Init("Green Den Spider Hatchling",
                 new State("Attack",
+                    new Wander(0.1f),
                     new Protect(0.4f, "Green Den Spider Hatchling"),
                     new Shoot(5, 1, 0, 0)
                 )
                 );
+
+            db.Init("Black Den Spider",
+                     new State("Wander",
+                         new StayAbove(0.2f, 50),
+                         new Wander(0.4f),
+                         new PlayerWithinTransition(7, true, "Attack")
+                             ),
+                     new State("Attack",
+                         new StayAbove(0.2f, 50),
+                         new Prioritize(
+                             new Charge(2),
+                             new Wander(0.4f)
+                             ),
+                         new Shoot(5, predictive: 1),
+                         new TimedTransition(1000, "Wander")
+                         ),
+                   new Threshold(0.01f, 
+                        new ItemLoot("Healing Ichor", .05f)
+                       )
+                );
+
+            db.Init("Brown Den Spider",
+                    new State("Idle",
+                        new StayAbove(0.2f, 50),
+                        new Wander(0.4f),
+                        new PlayerWithinTransition(7, true, "Attack")
+                        ),
+                    new State("Attack",
+                        new Prioritize(
+                            new StayAbove(0.4f, 160),
+                            new Follow(0.9f, acquireRange: 9, range: 0),
+                            new Wander(0.4f)
+                            ),
+                        new Shoot(8, count: 3, shootAngle: 10, cooldown: 400),
+                        new TimedTransition(10000, "Idle")
+                        ),
+                    new Threshold(0.01f,
+                        new ItemLoot("Healing Ichor", .05f)
+                    )
+                    );
+            db.Init("Black Spotted Den Spider",
+                new State("base",
+                    new Wander(0.4f),
+                    new Shoot(7)
+                    ),
+                new Threshold(0.01f,
+                    new ItemLoot("Healing Ichor", .05f)
+
+                )
+                 );
+            db.Init("Red Spotted Den Spider",
+                new State("base",
+                    new Prioritize(
+                    new Follow(0.3f, acquireRange: 10, range: 4),
+                    new Wander(0.5f)
+                    ),
+                    new Shoot(10)
+                    ),
+                new Threshold(0.01f,
+                  new ItemLoot("Healing Ichor", .05f)
+                )
+                    );
+
         }
     }
 }
